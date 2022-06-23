737ce171a81ce986ffe99adc91fb03cecb79e3d8
iago0707 <61835860+iago0707@users.noreply.github.com>
2022-06-23 11:28:20 -0300

    Add files via upload

--- component/read.jsx
+++ component/read.jsx
@@ -3,7 +3,7 @@ import { useState,useEffect} from 'react'
 
 //importar a config do firebase
 import { app, database } from '../services/firebase'
-import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc, getDoc } from 'firebase/firestore';
+import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc, getDoc, updateDoc } from 'firebase/firestore';
 
 //configurar o Firebase do projeto
 const contato = collection(database,'contato')
@@ -54,13 +54,42 @@ export default function Read() {
     }
 
     if(mensagem!=""){
-      setMostrar(false)
+      setMostrar(true)
     }
   }
   useEffect(()=>{
     show(ID)
   },[ID])
 
+  const bt_alterar = (id)=>{
+    const contatoShow = doc(database,'contato',id)
+    updateDoc(contatoShow,{
+      nome:nome,
+      email:email,
+      telefone:telefone,
+      mensagem:mensagem,
+
+    }).then(()=>{
+    setMostrar(false)
+    setNome("")
+    setTelefone("")
+    setEmail("")
+    setMensagem("")
+    setID(null)
+    read()
+
+    })
+  }
+
+  const bt_cancelar = ()=>{
+    setMostrar(false)
+    setNome("")
+    setTelefone("")
+    setEmail("")
+    setMensagem("")
+    setID(null)
+   }
+  
   return (
     <>
 
@@ -71,7 +100,8 @@ export default function Read() {
         <input type="email" name="email" placeholder='Email' className='form-control' id="" required onChange={event=>setEmail(event.target.value)} value={email} />
         <input type="tel" name="telefone" placeholder='Telefone' className='form-control' id="" required onChange={event=>setTelefone(event.target.value)} value={telefone} />
         <textarea name="mensagem" className='form-control' placeholder='Mensagem' id="" onChange={event=>setMensagem(event.target.value)} value={mensagem} ></textarea>
-        <input type="submit" value="SALVAR"  className='form-control btn btn-outline-dark' />
+        <input type="button" value="CANCELAR" className="form-control btn btn-outline-danger" onClick={bt_cancelar} />
+        <input type="submit" value="SALVAR"  className='form-control btn btn-outline-dark' onClick={()=>bt_alterar(contatoUnico.id)}/>
      
       </div>
     ):(
